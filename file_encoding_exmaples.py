7f7992be5c996e9e8f881c4bc5b97c0600002794
Gurubarron <70693597+Gurubarron@users.noreply.github.com>
2021-06-22 19:26:20 +0530

    Update file_encoding_exmaples.py

--- file_encoding_exmaples.py
+++ file_encoding_exmaples.py
@@ -33,6 +33,24 @@ with open('automate_data/billboard.csv', 'rb') as file:
 
 suggestion = UnicodeDammit(content)
 suggestion.original_encoding
+
+
+import os, shutil, glob
+
+#Source file 
+sourcefile = 'Desktop/00/'
+
+# for loop then I split the names of the image then making new folder 
+for file_path in glob.glob(os.path.join(sourcefile, '*.jpg*')):
+    new_dir = file_path.rsplit('.', 1)[0]    
+    # If folder does not exist try making new one
+    try:
+        os.mkdir(os.path.join(sourcefile, new_dir))
+    # except error then pass
+    except WindowsError:
+        pass
+    # Move the images from file to new folder based on image name
+    shutil.move(file_path, os.path.join(new_dir, os.path.basename(file_path)))
 #'iso-8859-1'
 
 opt5:
